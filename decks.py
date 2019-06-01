4e03498793418ea4c28251f8dc9c4de9cc21aefc
alex <alex.giorev@gmail.com>
2019-06-01 21:10:13 +0300

    i need to work on my git skills

--- decks.py
+++ decks.py
@@ -1,3 +1,5 @@
+import itertools
+import random
 import sqlite3
 
 import utils
@@ -10,15 +12,17 @@ class Deck:
     - self.id: a unique (among all other decks) integer which is used
       as the connection between the instance and the database.
     - self.name
+    - self.parent: the parent deck. None if the current deck is a root.
     - self.subdecks: a dict which maps names to decks (with the same name)
     - self.cards: a dict which maps card ids to cards
     - self.conn: a connection to the database which contains the
       persistent storage of the deck
     """
 
-    def __init__(self, id, name, conn):
+    def __init__(self, id, name, conn, parent):
         self.id = id
         self.name = name
+        self.parent = parent
         self.subdecks = {}
         self.cards = {}
         self.conn = conn
@@ -33,5 +37,39 @@ class Deck:
 
     @property
     def due_cards(self):
-        # returns a list of all cards which are up for review
-        raise NotImplementedError
+        """Returns a list of all cards which are up for review"""
+        today = utils.today()
+        def is_due(card):
+            card.due <= today
+        return [card for card in self.all_cards if is_due(card)]
+
+    @property
+    def all_cards(self):
+        """Returns a list of all cards in the deck (including those in
+        the deck's subdeck). The order of the cards is random"""
+        result = list(self._cards_iter)
+        random.shuffle(result)
+        return result
+    
+    @property
+    def _cards_iter(self):
+        """Returns an iterator of all cards in the deck (including the
+        deck's subdecks). The order of the cards is not specified."""
+        for card in self.cards.values():
+            yield card
+        for subdeck in self.subdecks.values():
+            for card in subdeck._cards_iter:
+                yield card
+
+    @property
+    def subdecks_iter(self):
+        """Just a utility function which returns an iterator of all
+        subdecks of @self. Using this iterator, a child is always
+        yielded before it's parent."""
+        
+        for child in self.subdecks.values():
+            for deep_subdeck in child.subdecks_iter:
+                yield deep_subdeck
+        yield self
+        
+        
--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import time
 
 def today():
     # returns the number of days since the epoch
-    return time.time() // (60 * 60 * 24)
+    return int(time.time()) // (60 * 60 * 24)
 
 def getid(conn, table):
     # returns a unique card id
     maxid = conn.execute(f'select max(id) from {table}').fetchone()[0]
-    return maxid + 1
+    return maxid + 1 if maxid is not None else 1
