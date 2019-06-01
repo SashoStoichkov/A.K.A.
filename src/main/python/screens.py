95fe7867bbc3c9135a676597d1dbf11d58a1d3b0
SashoStoichkov <sashostoichkov@gmail.com>
2019-06-01 19:36:58 +0300

    add cards view

--- src/main/python/screens.py
+++ src/main/python/screens.py
@@ -97,7 +97,7 @@ class DeckInfoScreen(p.QtWidgets.QDialog):
 
         deleteCardButton = p.QtWidgets.QPushButton(self)
         deleteCardButton.setText('Delete Card')
-        deleteCardButton.clicked.connect(self.goToStart)
+        deleteCardButton.clicked.connect(self.deleteCard)
         deleteCardButton.setMinimumSize(100, 50)
         top_layout.addWidget(deleteCardButton, alignment=p.QtCore.Qt.AlignCenter)
         top_layout.setContentsMargins(1, 1, 1, 1)
@@ -119,3 +119,56 @@ class DeckInfoScreen(p.QtWidgets.QDialog):
     def goToStart(self):
         self.start = StartScreen()
         self.start.show()
+
+    def deleteCard(self):
+        self.choice = p.QtWidgets.QMessageBox.question(self, "Delete Card!", "Are you sure you want to delete a Card?", p.QtWidgets.QMessageBox.Yes | p.QtWidgets.QMessageBox.No)
+
+        if self.choice == p.QtWidgets.QMessageBox.Yes:
+            print("Deleted!")
+        else:
+            print("You keep it... for now!")
+
+class CardsScreen(p.QtWidgets.QDialog):
+    def __init__(self, deckname, parent=None):
+        super(CardsScreen, self).__init__(parent)
+
+        self.originalPalette = p.QtWidgets.QApplication.palette()
+
+        self.setWindowTitle("{0} Cards".format(deckname))
+
+        title = p.QtWidgets.QLabel("{0} Cards:".format(deckname))
+        title.setFont(p.QtGui.QFont("Times", 50))
+
+        top_layout = p.QtWidgets.QHBoxLayout()
+        top_layout.addWidget(title)
+
+        self.list = p.QtWidgets.QListView(self)
+        top_layout.addWidget(self.list)
+        self.model = p.QtGui.QStandardItemModel(self.list)
+
+        # dummy data
+        foods = [
+            'Cookie dough', # Must be store-bought
+            'Hummus', # Must be homemade
+            'Spaghetti', # Must be saucy
+            'Dal makhani', # Must be spicy
+            'Chocolate whipped cream' # Must be plentiful
+        ]
+
+        self.list.setModel(self.model)
+        self.list.setMinimumSize(600, 400)
+
+        for food in foods:
+            item = p.QtGui.QStandardItem(food)
+            item.setFont(p.QtGui.QFont("Times", 25))
+
+            self.model.appendRow(item)
+
+            self.list.setIndexWidget(item.index(), DeckInfoButton(food))
+
+        main_layout = p.QtWidgets.QGridLayout()
+        main_layout.addLayout(top_layout, 0, 0, 1, 1)
+
+        self.setLayout(main_layout)
+
+        self.showMaximized()
