81e07922808320ba2d070d02686693a80ecb29e5
= <kamen.hristov.98@gmail.com>
2019-06-02 01:26:05 +0300

    deleted some files

--- create_db.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker
-
-import const
-from models import Deck, Card, Base
-
-if __name__ == "__main__":
-    engine = create_engine(f'sqlite:///{const.DB_NAME}')
-    Base.metadata.create_all(engine)
