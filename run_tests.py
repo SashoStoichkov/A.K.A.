e2aa1dcade7cf600087c717866aa5c4c8ae2282b
= <kamen.hristov.98@gmail.com>
2019-06-02 18:26:57 +0300

    updated file

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,9 @@
 import unittest
-from tests.test_collection import TestLoader, TestCollection
+from tests.test_collection import TestLoader, TestCreateDecks, TestFindDeck, TestRemoveDeck
 from stub_manager import StubManager
 
 if __name__ == "__main__":
-    test_cases = [TestLoader, TestCollection]
+    test_cases = [TestRemoveDeck, TestLoader, TestFindDeck, TestCreateDecks]
     for suite in [test_case.build_suite() for test_case in test_cases]:
         with StubManager():
             unittest.TextTestRunner().run(suite)
