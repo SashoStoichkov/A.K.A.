953be3d863293d78dbe63efb826248f71428518d
= <kamen.hristov.98@gmail.com>
2019-06-02 15:30:25 +0300

    uncomented tests xD

--- tests/test_collection.py
+++ tests/test_collection.py
@@ -39,7 +39,6 @@ class TestLoader(unittest.TestCase):
         return test_suite
 
 
-
 class TestCollection(unittest.TestCase):
     def setUp(self):
         self.decks_for_cleaning = [('text-editors', ), ('atom',), ('C++', )]
@@ -58,43 +57,43 @@ class TestCollection(unittest.TestCase):
     def test_when_create_decks_is_called_with_an_empty_string_raise_value_error(self):
         self.assertRaises(ValueError, self.collection.create_decks, "")
      
-    # def test_when_create_decks_is_called_with_new_root_deck_return_expected_result(self):
-    #     new_deck = 'text-editors'
-    #     self.collection.create_decks(new_deck)
-    #     expected_result = [
-    #         (0, 'main', None),
-    #         (1, 'prog-langs', 0), 
-    #         (2, 'Python', 1), 
-    #         (3, 'C', 1),
-    #         (4, 'text-editors', 0)
-    #     ]
-    #     self.assertEqual(self.loader.deck_rows, expected_result)
+    def test_when_create_decks_is_called_with_new_root_deck_return_expected_result(self):
+        new_deck = 'text-editors'
+        self.collection.create_decks(new_deck)
+        expected_result = [
+            (0, 'main', None),
+            (1, 'prog-langs', 0), 
+            (2, 'Python', 1), 
+            (3, 'C', 1),
+            (4, 'text-editors', 0)
+        ]
+        self.assertEqual(self.loader.deck_rows, expected_result)
         
 
-    # def test_when_create_decks_is_called_with_new_subdeck_of_proglangs_return_expected_result(self):
-    #     new_deck = 'prog-langs::C++'
-    #     self.collection.create_decks(new_deck)
-    #     expected_result = [
-    #         (0, 'main', None),
-    #         (1, 'prog-langs', 0), 
-    #         (2, 'Python', 1), 
-    #         (3, 'C', 1),
-    #         (4, 'C++', 1)
-    #     ]
-    #     self.assertEqual(self.loader.deck_rows, expected_result)
+    def test_when_create_decks_is_called_with_new_subdeck_of_proglangs_return_expected_result(self):
+        new_deck = 'prog-langs::C++'
+        self.collection.create_decks(new_deck)
+        expected_result = [
+            (0, 'main', None),
+            (1, 'prog-langs', 0), 
+            (2, 'Python', 1), 
+            (3, 'C', 1),
+            (4, 'C++', 1)
+        ]
+        self.assertEqual(self.loader.deck_rows, expected_result)
     
-    # def test_when_create_decks_is_called_with_new_root_and_subdeck_return_expected_result(self):
-    #     new_deck = 'text-editors::atom'
-    #     self.collection.create_decks(new_deck)
-    #     expected_result = [
-    #         (0, 'main', None),
-    #         (1, 'prog-langs', 0), 
-    #         (2, 'Python', 1), 
-    #         (3, 'C', 1),
-    #         (4, 'text-editors', 0),
-    #         (5, 'atom', 4)
-    #     ]
-    #     self.assertEqual(self.loader.deck_rows, expected_result)
+    def test_when_create_decks_is_called_with_new_root_and_subdeck_return_expected_result(self):
+        new_deck = 'text-editors::atom'
+        self.collection.create_decks(new_deck)
+        expected_result = [
+            (0, 'main', None),
+            (1, 'prog-langs', 0), 
+            (2, 'Python', 1), 
+            (3, 'C', 1),
+            (4, 'text-editors', 0),
+            (5, 'atom', 4)
+        ]
+        self.assertEqual(self.loader.deck_rows, expected_result)
     
     
     @classmethod
