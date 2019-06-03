308f0199a2c4da5fe29514e06febcc1b754c81aa
= <kamen.hristov.98@gmail.com>
2019-06-03 16:32:46 +0300

    cleaned main

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-import os
-import sys
-from fbs_runtime.application_context import ApplicationContext
-
-import screens
-import collection
-import const
-
 if __name__ == "__main__":
-    # os.chdir('/home/alex/A.K.A./src/main/python')
-    col = collection.Loader(const.DB_NAME).load()
-    screens.init(col)
-    app = ApplicationContext()
-    s = screens.StartScreen()
-    s.show()
-    sys.exit(app.app.exec_())
+    pass
